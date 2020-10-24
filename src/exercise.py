95a27610209388779ac8559e54d05a3ad0b6dd40
pythonstudent01 <python-student@den01.co.uk>
2020-10-24 10:17:31 +0000

    Attempt solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #write your code below this line
+    first = int(input("Give the first number:"))
+    second = int(input("Give the second number:"))
+
+    if first > second:
+      greatest = first
+      print("Greater number is: " + str(greatest))
+    elif second > first:
+      greatest = second
+      print("Greater number is: " + str(greatest))
+    else:
+      print("The numbers are equal!")
 
 if __name__ == '__main__':
     main()
